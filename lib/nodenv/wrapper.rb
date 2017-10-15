54ff259f280bf2c3a436dfa3d7ffb8e055ce344e
Albert Bertram <bertrama@umich.edu>
2017-10-14 21:45:46 -0400

    Execute bundle gem nodenv-wrapper.

---
+++ lib/nodenv/wrapper.rb
@@ -0,0 +1,7 @@
+require "nodenv/wrapper/version"
+
+module Nodenv
+  module Wrapper
+    # Your code goes here...
+  end
+end
---
+++ lib/nodenv/wrapper/version.rb
@@ -0,0 +1,5 @@
+module Nodenv
+  module Wrapper
+    VERSION = "0.1.0"
+  end
+end
