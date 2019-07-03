e40187de966707f6db903ac362716a6c89ff3f9c
Albert Bertram <bertrama@umich.edu>
2019-07-03 12:30:34 -0400

    Maybe this will work better with moku.

--- lib/nodenv/wrapper.rb
+++ lib/nodenv/wrapper.rb
@@ -3,5 +3,10 @@ require_relative "wrapper/version"
 module Nodenv
   module Wrapper
     NODE_VERSION = '8.6.0'
+    NPM_PATH  = "ext/nodenv/versions/#{NODE_VERSION}/bin/npm"
+    NODE_PATH = "ext/nodenv/versions/#{NODE_VERSION}/bin/node"
+
+    NPM  = File.expand_path("../../../#{NODE_PATH}", __FILE__)
+    NODE = File.expand_path("../../../#{NODE_PATH}", __FILE__)
   end
 end
